74bd72d22689910f2797b18b425764ac4958be40
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2023-02-08 17:26:08 -0300

    React props and using proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import Header from "./Header";
+import Post from "./Post";
 
 function App() {
   return (
     <>
-      <h1>Component App !!!!</h1>
-      <h2>Subtitle</h2>
+      <Header>
+        <h2>Posts da semana</h2>
+      </Header>
+
+      <hr/>
+
+      <Post
+        likes={20}
+        post={{
+          title: 'Título da notícia 01',
+          subtitle: 'Subtítulo da notícia 01'
+        }}
+      />
+
+      <Post
+        likes={10}
+        post={{
+          title: 'Título da notícia 02',
+          subtitle: 'Subtítulo da notícia 02'
+        }}
+      />
+
+      <Post
+        likes={50}
+        post={{
+          title: 'Título da notícia 03',
+          subtitle: 'Subtítulo da notícia 03'
+        }}
+      />
     </>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+export default function Header({title, children}) {
+  return(
+    <>
+      <h1>{title}</h1>
+      {children}
+    </>
+  );
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+}
+
+Header.defaultProps = {
+  title: `JStack's Blog`
+}
