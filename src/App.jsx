72650f9e94cc0998405203efba58e84ce3ea1e8d
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2023-02-09 10:05:20 -0300

    list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import Header from "./Header";
 import Post from "./Post";
 
+const posts = [
+  {
+    title: 'Title#01',
+    subtitle: 'Subtitle#01',
+    likes: 20
+  },
+  {
+    title: 'Title#02',
+    subtitle: 'Subtitle#02',
+    likes: 10
+  },
+  {
+    title: 'Title#03',
+    subtitle: 'Subtitle#03',
+    likes: 50
+  },
+]
+
 function App() {
   return (
     <>
@@ -11,29 +29,18 @@ function App() {
 
       <hr/>
 
-      <Post
-        likes={20}
-        post={{
-          title: 'Título da notícia 01',
-          subtitle: 'Subtítulo da notícia 01'
-        }}
-      />
-
-      <Post
-        likes={10}
-        post={{
-          title: 'Título da notícia 02',
-          subtitle: 'Subtítulo da notícia 02'
-        }}
-      />
-
-      <Post
-        likes={50}
-        post={{
-          title: 'Título da notícia 03',
-          subtitle: 'Subtítulo da notícia 03'
-        }}
-      />
+      {
+        posts.map(post=> (
+          <Post
+            key={post.title}
+            likes={post.likes}
+            post={{
+              title:post.title,
+              subtitle: post.subtitle
+            }}
+          />
+        ))
+      }
     </>
   );
 };
