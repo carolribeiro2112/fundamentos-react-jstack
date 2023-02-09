9358b7f35158c53485a4b425011a3d90abe253c9
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2023-02-09 10:50:36 -0300

    working with states

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Post from "./Post";
 
-const posts = [
-  {
-    title: 'Title#01',
-    subtitle: 'Subtitle#01',
-    likes: 20
-  },
-  {
-    title: 'Title#02',
-    subtitle: 'Subtitle#02',
-    likes: 10
-  },
-  {
-    title: 'Title#03',
-    subtitle: 'Subtitle#03',
-    likes: 50
-  },
-]
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: Math.random(),
+      title: 'Title#01',
+      subtitle: 'Subtitle#01',
+      likes: 20
+    },
+    {
+      id: Math.random(),
+      title: 'Title#02',
+      subtitle: 'Subtitle#02',
+      likes: 10
+    },
+    {
+      id: Math.random(),
+      title: 'Title#03',
+      subtitle: 'Subtitle#03',
+      likes: 50
+    },
+  ]);
+
+  function handleRefresh() {
+    setTimeout(() => {
+      setPosts((prevState) => [
+        ...prevState, 
+        {
+          id: Math.random(),
+          title: `Title#0${prevState.length + 1}`,
+          subtitle: `Subtitle#0${prevState.length + 1}`,
+          likes: 50
+        },
+      ]);
+    }, 2000)
+  };
+
   return (
     <>
       <Header>
-        <h2>Posts da semana</h2>
+        <h2>
+          Posts da semana
+
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
 
       <hr/>
@@ -32,7 +54,7 @@ function App() {
       {
         posts.map(post=> (
           <Post
-            key={post.title}
+            key={post.id}
             likes={post.likes}
             post={{
               title:post.title,
